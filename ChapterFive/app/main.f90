3d0fe86c24577c420fe21c0bfb00550840166758
Angel Mosquera <angelm@wolfram.com>
2024-08-28 01:26:12 -0500

    gain of stocks

--- ChapterFive/app/main.f90
+++ ChapterFive/app/main.f90
@@ -1,6 +1,31 @@
 program main
-  use ChapterFive, only: say_hello
+  use iso_fortran_env,only: int32, real32
+  use ChapterFive,only: readStock, alloc, free, reverse
   implicit none
 
-  call say_hello()
+  integer :: n
+  character(len=:),allocatable :: fileDir, fileName
+  character(len=4), allocatable :: tags(:)
+  character(len=:), allocatable :: time(:)
+  real, allocatable :: open(:), high(:), low(:), close(:), adjclose(:), volume(:)
+  real :: gain
+  fileDir ="C:/Users/Angel/Documents/Trabajo de Investigación/ModernFortran/ChapterFive/stock-prices/data"
+  tags =["AAPL","AMZN","CRAY","CSCO","HPQ ",&
+         "IBM ","INTC","MSFT","NVDA","ORCL"]
+  
+  do n = 1, size(tags)
+    fileName = fileDir//"/" // trim(tags(n))//".csv"
+    call readStock(fileName, time, open, high, low, close,adjclose, volume)
+    adjclose = reverse(adjclose)
+    gain = adjclose(size(adjclose))- adjclose(1)
+
+    if (n == 1) then
+      print *, time(size(time)) // ' through ' // time(1)
+      print *, 'Symbol, Gain (USD), Relative gain (%)'
+      print *, '-------------------------------------'
+    end if
+    
+    print *, tags(n), gain, nint(gain / adjclose(1) * 100)
+  end do
+
 end program main
--- ChapterFive/src/ChapterFive.f90
+++ ChapterFive/src/ChapterFive.f90
@@ -1,10 +1,86 @@
 module ChapterFive
+  use iso_fortran_env, only: real32
   implicit none
   private
 
-  public :: say_hello
+  public :: say_hello, readStock, alloc, free, reverse
 contains
   subroutine say_hello
     print *, "Hello, ChapterFive!"
   end subroutine say_hello
+
+  subroutine readStock(filename, time, open, high, low, close,adjclose, volume)
+    character(*), intent(in):: filename
+    character(:),allocatable, intent(inout):: time(:)
+    real(real32), allocatable, intent(inout) ::open(:),high(:),low(:),close(:),adjclose(:), volume(:)
+
+    integer :: fileUnit
+    integer :: n, nm
+
+    nm = numRecords(filename)-1
+
+    if (allocated(time)) deallocate(time)
+    allocate(character(10) :: time(nm))
+    call alloc(open, nm)
+    call alloc(high, nm)
+    call alloc(low, nm)
+    call alloc(close, nm)
+    call alloc(adjclose, nm)
+    call alloc(volume, nm)
+
+    open(newunit = fileUnit, file=filename)
+    read(fileUnit, fmt=*, end=1)
+
+    do n = 1, nm
+      read(fileUnit, fmt=*, end=1) &
+            time(n), open(n), high(n), low(n), close(n), adjclose(n),volume(n)
+    end do
+
+    1 close(fileUnit)
+
+  end subroutine readStock
+
+  integer function numRecords(fileName)
+    character(len=*), intent(in) :: fileName
+    integer :: fileUnit
+    open(newunit=fileUnit, file=fileName)
+    numRecords = 0
+    do
+      read(unit=fileUnit, fmt=*, end=1)
+      numRecords = numRecords + 1
+    end do
+    1 continue
+    close(unit=fileUnit)
+  end function numRecords
+
+  subroutine alloc(a, arrSize)
+    real, allocatable, intent(inout) :: a(:)
+    integer :: arrSize
+    integer:: stat
+    character(len=100) :: errmsg
+
+    if (allocated(a)) call free(a)
+    allocate(a(arrSize), stat=stat, errmsg=errmsg)
+    if(stat > 0) error stop errmsg
+
+  end subroutine alloc
+
+  subroutine free(a)
+    real, allocatable, intent(inout):: a(:)
+    integer:: stat
+    character(len=100):: errmsg
+
+    if(.not. allocated(a)) return
+    deallocate(a, stat=stat, errmsg=errmsg)
+    if (stat>0) then
+      error stop errmsg
+    end if
+
+  end subroutine free
+
+  pure function reverse(a)
+    real, intent(in):: a(:)
+    real :: reverse(size(a))
+    reverse = a(size(a):1:-1)
+  end function reverse
 end module ChapterFive
