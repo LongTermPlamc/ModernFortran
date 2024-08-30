c5da9e5e1c327ed9677649d43949a0cd36e412ec
Angel Mosquera <angelm@wolfram.com>
2024-08-30 03:52:55 -0500

    challenge 2 of chapter five done

--- ChapterFive/app/main.f90
+++ ChapterFive/app/main.f90
@@ -1,31 +1,56 @@
 program main
   use iso_fortran_env,only: int32, real32
-  use ChapterFive,only: readStock, alloc, free, reverse
+  use ChapterFive,only: readStock, alloc, free, reverse, mean, std, movingAverage, movingStd, writeStock
   implicit none
 
-  integer :: n
-  character(len=:),allocatable :: fileDir, fileName
+  integer :: n, len
+  character(len=:),allocatable :: fileDir, writeDir, fileName, newFileName
   character(len=4), allocatable :: tags(:)
   character(len=:), allocatable :: time(:)
   real, allocatable :: open(:), high(:), low(:), close(:), adjclose(:), volume(:)
-  real :: gain
+  real, allocatable ::  price(:), movAvg(:), movStd(:)
+  real :: gain, meanOfArray, stdOfArray
   fileDir ="C:/Users/Angel/Documents/Trabajo de Investigación/ModernFortran/ChapterFive/stock-prices/data"
+  writeDir ="C:/Users/Angel/Documents/Trabajo de Investigación/ModernFortran/ChapterFive/myData"
   tags =["AAPL","AMZN","CRAY","CSCO","HPQ ",&
          "IBM ","INTC","MSFT","NVDA","ORCL"]
-  
-  do n = 1, size(tags)
-    fileName = fileDir//"/" // trim(tags(n))//".csv"
-    call readStock(fileName, time, open, high, low, close,adjclose, volume)
-    adjclose = reverse(adjclose)
-    gain = adjclose(size(adjclose))- adjclose(1)
+  !! Relative gain example
+  ! do n = 1, size(tags)
+  !   fileName = fileDir//"/" // trim(tags(n))//".csv"
+  !   call readStock(fileName, time, open, high, low, close,adjclose, volume)
+  !   adjclose = reverse(adjclose)
+  !   gain = adjclose(size(adjclose))- adjclose(1)
 
-    if (n == 1) then
-      print *, time(size(time)) // ' through ' // time(1)
-      print *, 'Symbol, Gain (USD), Relative gain (%)'
-      print *, '-------------------------------------'
-    end if
+  !   if (n == 1) then
+  !     print *, time(size(time)) // ' through ' // time(1)
+  !     print *, 'Symbol, Gain (USD), Relative gain (%)'
+  !     print *, '-------------------------------------'
+  !   end if
     
-    print *, tags(n), gain, nint(gain / adjclose(1) * 100)
+  !   print *, tags(n), gain, nint(gain / adjclose(1) * 100)
+  ! end do
+
+  !! Volatility analisys
+  do n=1, size(tags)
+    fileName = fileDir//"/" // trim(tags(n))//".csv"
+    newFileName = writeDir//"/" // trim(tags(n))//"Volatility.csv"
+    call readStock(filename, time, open,high, low, close, adjclose, volume)
+
+    len = size(time)
+
+    call alloc(price, len)
+    price = reverse(adjclose)
+    call alloc(movAvg, len)
+    movAvg = movingAverage(adjclose, 30)
+    call alloc(movStd, len)
+    movStd = movingStd(adjclose, 30)
+    
+    time = time(len:1:-1)
+    call writeStock(newFileName, time , price, movAvg, movStd)
+
+    call free(price)
+    call free(movAvg)
+    call free(movStd)
   end do
 
 end program main
--- ChapterFive/src/ChapterFive.f90
+++ ChapterFive/src/ChapterFive.f90
@@ -1,9 +1,9 @@
 module ChapterFive
-  use iso_fortran_env, only: real32
+  
   implicit none
   private
 
-  public :: say_hello, readStock, alloc, free, reverse
+  public :: say_hello, readStock, alloc, free, reverse, mean, std, movingAverage, movingStd, writeStock
 contains
   subroutine say_hello
     print *, "Hello, ChapterFive!"
@@ -12,7 +12,7 @@ contains
   subroutine readStock(filename, time, open, high, low, close,adjclose, volume)
     character(*), intent(in):: filename
     character(:),allocatable, intent(inout):: time(:)
-    real(real32), allocatable, intent(inout) ::open(:),high(:),low(:),close(:),adjclose(:), volume(:)
+    real, allocatable, intent(inout) ::open(:),high(:),low(:),close(:),adjclose(:), volume(:)
 
     integer :: fileUnit
     integer :: n, nm
@@ -39,6 +39,21 @@ contains
     1 close(fileUnit)
 
   end subroutine readStock
+
+  subroutine writeStock(filename, time, price, movAverage, movStdDev)
+    character(*), intent(in) :: filename
+    character(len=:), allocatable, intent(in) :: time(:)
+    real, intent(in) :: price(:), movAverage(:), movStdDev(:)
+    integer ::newUnit, index
+
+    open(newUnit, file=filename, action='write')
+
+    do index =1, size(time)
+      write(newUnit, *) time(index), price(index),movAverage(index),movStdDev(index)
+    end do
+    close(newUnit)
+
+  end subroutine
 
   integer function numRecords(fileName)
     character(len=*), intent(in) :: fileName
@@ -83,4 +98,47 @@ contains
     real :: reverse(size(a))
     reverse = a(size(a):1:-1)
   end function reverse
+
+  pure function mean(a)
+    real, intent(in) :: a(:)
+    real:: mean
+
+    mean = sum(a) / size(a)
+  end function mean
+
+  pure real function std(a)
+    real, intent(in) :: a(:)
+    real :: tempMean
+
+    tempMean = mean(a)
+
+    std = sqrt(mean((a-tempMean) ** 2)) 
+
+  end function std
+
+  function movingAverage(a, win)
+    real, intent(in) :: a(:)
+    integer, intent(in) :: win
+    real :: movingAverage(size(a))
+    integer :: i, winSize
+
+    do i = 1, size(a)
+      winSize = max(i-win,1)
+      movingAverage(i) = mean(a(winSize:i))
+    end do
+  end function movingAverage
+
+  function movingStd(a, win)
+    real, intent(in) :: a(:)
+    integer, intent(in) :: win
+    real :: movingStd(size(a))
+    integer :: i, winSize
+
+    do i = 1, size(a)
+      winSize = max(i-win,1)
+      movingStd(i) = std(a(winSize:i))
+    end do
+  end function movingStd
+
+
 end module ChapterFive
